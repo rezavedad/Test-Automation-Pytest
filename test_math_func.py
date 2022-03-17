f5f420af9b16b11af261555b937f7b09eb87425e
vedadrez <reza.hv@outlokk.de>
2022-03-17 10:36:38 +0100

    pytest commands

--- test_math_func.py
+++ test_math_func.py
@@ -1,17 +1,20 @@
 # test_ will help the python to recognize this ass a test
+# in Terminal: cls to clear terminal
 # pytest -v -p no:warnings
 
 import pytest
 import math_func
-
+import sys # getting version of python
 
 # all unit tests must have this test_ at the beginning
-@pytest.mark.number   # decorator
+# @pytest.mark.number   # decorator
+# @pytest.mark.skip(reason="Do not execute this test unit at the moment. (Reza Vedad) ")
+# @pytest.mark.skipif(sys.version_info > (3, 3) , reason="Version not supported.")
 def test_add() :
     assert math_func.add(7, 3) == 10
     assert math_func.add(7) == 9
 
-@pytest.mark.number
+# @pytest.mark.number
 def test_product() :
     assert math_func.product(5, 5) ==25
     assert math_func.product(5) == 10
@@ -20,7 +23,7 @@ def test_product() :
 # in Terminal : pytest test_math_func.py -v  ,  v: verbose for more info
 # in Terminal : pytest or pytest -v (test_ already in name)
 
-@pytest.mark.string
+# @pytest.mark.string
 def test_add_strings() :
     result = math_func.add('hello', ' world')
     assert result == 'hello world'
@@ -28,7 +31,7 @@ def test_add_strings() :
     assert 'hekllo' not in result
 
 
-@pytest.mark.string
+# @pytest.mark.string
 def test_product_strings() :
     assert math_func.product('hello ', 3) == 'hello hello hello '
     result = math_func.product('hello ')
@@ -57,3 +60,9 @@ def test_product_strings() :
 # Warnings
 # pytest -v -W ignore::DeprecationWarning
 # pytest -v -p no:warnings  , no warnings at all
+
+# Skipping
+# @pytest.mark.number  change to : @pytest.mark.skip(reason="Do not execute this test unit at the moment. (Reza Vedad) ")
+# skip if
+# @pytest.mark.skipif(sys.version_info < (3, 3) , reason="Do not execute this test unit at the moment. (Reza Vedad) ")
+# python --version
